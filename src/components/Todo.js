7f7642cc35c3dfd7f8ebb3bf2bc4687954c75cc0
Faad Ragib <ragibfuad@gmail.com>
2022-10-08 16:01:59 +0600

    Basic functional component alongside class component

---
+++ src/components/Todo.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+const Todo = () => {
+	const [todo, setTodo] = useState("");
+	const [warning, setWarning] = useState(null);
+
+	const handleInput = (e) => {
+		const inputValue = e.target.value;
+		const updatedWarning = inputValue.includes(".js")
+			? "You need JS skill to complete the task. Do you have it?"
+			: null;
+		setTodo(inputValue);
+		setWarning(updatedWarning);
+	};
+	return (
+		<div>
+			<p>{todo}</p>
+			<p>
+				<textarea name="todo" value={todo} onChange={handleInput} />
+			</p>
+			<hr />
+			<h2>{warning || "Good choice"}</h2>
+		</div>
+	);
+};
+
+export default Todo;
