126aaa4cefa2ce935ed33fe0dc90906517182580
Faad Ragib <ragibfuad@gmail.com>
2022-10-08 16:25:12 +0600

    in index.js we wrote a useState function of our own

--- src/index.js
+++ src/index.js
@@ -2,9 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+let states = [];
+let stateIndex = -1;
+
+function useState(defaultValue) {
+	const index = ++stateIndex;
+	if (states[index]) {
+		return states[index];
+	}
+	const setValue = (newValue) => {
+		states[index][0] = newValue;
+		renderWithFuad();
+	};
+	const returnArray = [defaultValue, setValue];
+	states[index] = returnArray;
+	return returnArray;
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-);
+function renderWithFuad() {
+	stateIndex = -1;
+	root.render(
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	);
+}
+renderWithFuad();
